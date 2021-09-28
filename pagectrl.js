7b0f13a4e6630cc6fdce0d83be90f5b15634560d
yanaiFarm <90092604+yanaiFarm@users.noreply.github.com>
2021-09-28 21:14:53 +0900

    Add files via upload

--- pagectrl.js
+++ pagectrl.js
@@ -1,72 +1,89 @@
 $(function(){;
 });
 $(window).on('load', function(){
-  loadjs();
+  loadjs(0);
 });
 
-function loadjs(){
+
+function loadjs(mode){
+
+  // ラジオボタン
+  var sei_radio = document.getElementsByName("tab-head");
+  
   // チェックボックス全てを取得
   var chk = document.querySelectorAll("input[type='checkbox']");
   
   //select要素の取得
   var select = document.querySelector("#selectfact");
+  
   //option要素の取得（配列）
   var options = document.querySelectorAll("#selectfact option");
   
-  // ラジオボタン
-  var sei_radio = document.getElementsByName("tab-head");
-  
    // ラジオボタン
   var ped_radio = document.getElementsByName("tab");
+
+  var scroll_to_top_btn = document.querySelector('#scroll-to-top-btn');
+
   
   // 詳細リンク
   //var dtl_link = document.getElementsByClassName("clickable-row");
 
- 
-  // selectboxを選択した際のイベント取得
-  
-  select.addEventListener('change', (event) => {
-      let sei = 0;
-      let ped = 0;
 
-      for (let i = 0; i < sei_radio.length; i++) {
-        if (sei_radio[i].id == "1" && sei_radio[i].checked ) {
-          sei = 1;
-        }
-      }
+  //初期起動時のみイベントを追加
+  if (mode == 0) {
+	const pagetopBtn = document.querySelector('#page-top');
+	pagetopBtn.addEventListener('click', () => {
+		window.scrollTo({
+	    	top: 0,
+	    	behavior: "smooth"
+	  	});
+	});
+	
+	// 因子を選択した際のイベント取得
+	select.addEventListener('change', (event) => {
+	      let sei = 0;
+	      let ped = 0;
 
-  	  for (let i = 0; i < ped_radio.length; i++) {
-  		if ( ped_radio[i].checked ) {
-  			ped =  i;
-  		}
-  	  }
-  	  dispHorse(chk , sei, ped, select);
-  	  lisnerLink();
-  });
-  
-  // 全てチェックボックスを選択した際のイベント取得
-  for (let i = 0; i < chk.length; i++) {
+	      for (let i = 0; i < sei_radio.length; i++) {
+	        if (sei_radio[i].id == "1" && sei_radio[i].checked ) {
+	          sei = 1;
+	        }
+	      }
+	  	  for (let i = 0; i < ped_radio.length; i++) {
+	  		if ( ped_radio[i].checked ) {
+	  			ped =  i;
+	  		}
+	  	  }
+	  	  dispHorse(chk , sei, ped, select);
+	  	  console.count();
+	  	  lisnerLink();
+	});	  
 
-    chk[i].addEventListener('change', (event) => {
-      let sei = 0;
-      let ped = 0;
-    
-  	  for (let i = 0; i < sei_radio.length; i++) {
-  		if (sei_radio[i].id == "1" && sei_radio[i].checked ) {
-  			sei = 1;
-  		}
-  	  }
-  	  for (let i = 0; i < ped_radio.length; i++) {
-  		if ( ped_radio[i].checked ) {
-  			ped =  i;
-  		}
-  	  }
-  	  dispHorse(chk , sei, ped, select);
-  	  lisnerLink();
-      });
+	// 全てチェックボックスを選択した際のイベント取得
+	for (let i = 0; i < chk.length; i++) {
+
+	    chk[i].addEventListener('change', (event) => {
+	      let sei = 0;
+	      let ped = 0;
+	    
+	  	  for (let i = 0; i < sei_radio.length; i++) {
+	  		if (sei_radio[i].id == "1" && sei_radio[i].checked ) {
+	  			sei = 1;
+	  		}
+	  	  }
+	  	  for (let i = 0; i < ped_radio.length; i++) {
+	  		if ( ped_radio[i].checked ) {
+	  			ped =  i;
+	  		}
+	  	  }
+	  	  dispHorse(chk , sei, ped, select);
+	  	  console.count();
+	  	  lisnerLink();
+	      });
+	}
   }
 
-  // 全てチェックボックスを選択した際のイベント取得
+  // 性別オプションを選択した際のイベント取得
   for (let i = 0; i < sei_radio.length; i++) {
   
   	sei_radio[i].addEventListener('change', (event) => {
@@ -82,10 +99,13 @@ function loadjs(){
   				ped =  i;
   			}
   		}
-  		dispHorse(chk , sei, ped, select);
+  		
+  		formatHorse(sei);
+  		//dispHorse(chk , sei, ped, select);
   		lisnerLink();
   		});
   }
+
   lisnerLink(); 
 }
 
@@ -234,7 +254,7 @@ function dispHorse(chk, sei, ped, factor) {
   var mig_arr = [];
   var jik_arr = [];
   var ashi_arr = [];
-  var hosi_arr = [];
+  var rare_arr = [];
   var chk_idx = [];
   var hibon_arr = [];
   for (let i = 0; i < chk.length; i++) {
@@ -259,8 +279,8 @@ function dispHorse(chk, sei, ped, factor) {
           if ( chk[i].id.match(/^ashi\-/) != null) {
           	ashi_arr.push(chk[i].value);
           }
-          if ( chk[i].id.match(/^hosi\-/) != null) {
-          	hosi_arr.push(chk[i].value);
+          if ( chk[i].id.match(/^rare\-/) != null) {
+          	rare_arr.push(chk[i].value);
           }
           if ( chk[i].id.match(/^hibon/) != null) {
           	hibon_arr.push(1)
@@ -282,7 +302,7 @@ function dispHorse(chk, sei, ped, factor) {
      sessionStorage.setItem('factor_idx', factor.selectedIndex);
      
      // 絞り込み実施
-     filterHorse(t_arr, ht_arr, mig_arr, jik_arr,ashi_arr, hosi_arr, sei, hibon_arr ,factor.value);
+     filterHorse(t_arr, ht_arr, mig_arr, jik_arr,ashi_arr, rare_arr, sei, hibon_arr ,factor.value);
 }
 
 
